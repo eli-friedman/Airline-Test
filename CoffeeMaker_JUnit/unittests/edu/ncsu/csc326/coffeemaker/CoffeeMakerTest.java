050018471bf0a855b35333f9dda55683fe249f89
eli-friedman <efriedman92@gmail.com>
2015-12-08 16:50:49 -0500

    Comple

--- CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -8,6 +8,9 @@ import org.mockito.Mockito;
 
 import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
 
+//most of the methods in CoffeeMaker class just return methods in other classes so tests for 
+//them are done in respective classes. There were just a few that needed tested
+
 public class CoffeeMakerTest {
 	
 	CoffeeMaker myCM;
@@ -18,6 +21,7 @@ public class CoffeeMakerTest {
 	}
 	
 	//test add inventory method by adding some units of inventory
+	//tested all inventory types together because already tested individual methods in inventory class
 	//**Test fails because of error found in Inventory class addSugar method that uses a <= sign instead of >=
 	@Test
 	public void testAddInventory() throws InventoryException {
@@ -26,49 +30,90 @@ public class CoffeeMakerTest {
 	}
 
 	//testing the makeCoffee method which consists of 3 equivalence classes regarding the recipe price and amount paid: 
-	//1. where the recipe number is null 2. where the price of the recipe is less than or equal to the amount paid 3. 
-	//where the price of the recipe is more than the amount paid
+	//1. where the recipe number is null 2. where the amount paid is greater than or equal to the price of the recipe  3. 
+	//where the amount paid is less than the price of the recipe.
 	//there are also 2 equivalence classes for when the amount paid is sufficient: 1. the inventory is sufficient 2. the
 	//inventory is insufficient. (The default for all amounts in a recipe is 0 and coffeeMaker is instantiated with 15 of everything)
+	//also has test for edge case of exact change
+	
+	//asserting that first equivalence class (recipe is null) returns total amount paid
 	@Test
-	public void testMakeCoffee() {
+	public void testMakeCoffeeNull() {
+	
+		int amntPaid = 5;
 		
-		Recipe [] mockRecArray = new Recipe[3];
+		assertNull(myCM.getRecipes()[0]);
+		assertEquals( amntPaid, myCM.makeCoffee(0, amntPaid));
+		
+	}
+	
+	//Asserting that second equivalence class (amount paid is more than recipe price) returns the difference between
+	//amntPaid and the price
+	@Test
+	public void testMakeCoffeeGreater() {
+		
+		Recipe mockRec = new Recipe();
 		
 		//create the first mock recipe costing 4 and amount of coffee needed 20
-		mockRecArray[0] = Mockito.mock(Recipe.class);
-		Mockito.when(mockRecArray[0].getPrice()).thenReturn(4);
-		Mockito.when(mockRecArray[0].getAmtCoffee()).thenReturn(20);
+		mockRec = Mockito.mock(Recipe.class);
+		Mockito.when(mockRec.getPrice()).thenReturn(4);
 		
-		//create the second mock recipe costing 5 
-		mockRecArray[1] = Mockito.mock(Recipe.class);
-		Mockito.when(mockRecArray[1].getPrice()).thenReturn(5);
-		
-		//create the third mock recipe costing 6
-		mockRecArray[2] = Mockito.mock(Recipe.class);
-		Mockito.when(mockRecArray[2].getPrice()).thenReturn(6);
-		
-		// add recipes to coffeeMaker
-		myCM.addRecipe(mockRecArray[0]);
-		myCM.addRecipe(mockRecArray[1]);
-		myCM.addRecipe(mockRecArray[2]);
+		myCM.addRecipe(mockRec);
 		
 		int amntPaid = 5;
 		
-		//asserting that first equivalence class (recipe is null) returns total amount paid
-		assertEquals( amntPaid, myCM.makeCoffee(3, amntPaid));
-		
-		//Asserting that second equivalence class (amount paid is more than recipe price) but the 
-		//the recipe uses more coffee(20 units) than default(15 units) should return total amount paid
-		assertEquals( amntPaid, myCM.makeCoffee(0, amntPaid));
-		
-		//Asserting that second equivalence class (amount paid is more than recipe price) but the 
-		//the recipe uses less(0 units) than default(15 units) should return total amntPaid - price.
-		assertEquals( 0, myCM.makeCoffee(1, amntPaid));
-		
-		//Asserting that third equivalence class (amount paid is less than recipe price) returns total amntPaid
-		assertEquals( amntPaid, myCM.makeCoffee(2, amntPaid));
-		
+		assertEquals( 1, myCM.makeCoffee(0, amntPaid));
 	}
-
+	
+	//Asserting that second equivalence class (amount paid is less than recipe price) returns amntPaid
+	@Test
+	public void testMakeCoffeeLess() {
+		
+		Recipe mockRec = new Recipe();
+		
+		//create the second mock recipe costing 5 
+		mockRec = Mockito.mock(Recipe.class);
+		Mockito.when(mockRec.getPrice()).thenReturn(6);
+		
+		myCM.addRecipe(mockRec);
+		
+		int amntPaid = 5;
+		
+		assertEquals( amntPaid, myCM.makeCoffee(0, amntPaid));
+	}
+	
+	//Testing edge case where amount paid is exactly the recipe price. Should return 0
+		@Test
+		public void testMakeCoffeeExactChange() {
+			
+			Recipe mockRec = new Recipe();
+			
+			//create the second mock recipe costing 5 
+			mockRec = Mockito.mock(Recipe.class);
+			Mockito.when(mockRec.getPrice()).thenReturn(5);
+			
+			myCM.addRecipe(mockRec);
+			
+			int amntPaid = 5;
+			
+			assertEquals( 0, myCM.makeCoffee(0, amntPaid));
+		}
+	
+	//Asserting that when there is insufficient inventory MakeCoffee returns amntPaid
+	@Test
+	public void testMakeCoffeeInsufficientInv() {
+		
+		Recipe mockRec = new Recipe();
+		
+		//create the second mock recipe costing 5 
+		mockRec = Mockito.mock(Recipe.class);
+		Mockito.when(mockRec.getPrice()).thenReturn(4);
+		Mockito.when(mockRec.getAmtChocolate()).thenReturn(20);
+		
+		myCM.addRecipe(mockRec);
+		
+		int amntPaid = 5;
+		
+		assertEquals( amntPaid, myCM.makeCoffee(0, amntPaid));
+	}
 }
--- CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
+++ CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
@@ -19,69 +19,96 @@ public class InventoryTest {
 	}
 
 	//test both equivalence cases for setCocholate method: 1. amount >= 0     2. amount < 0
+	
+	//amount >= 0
 	@Test
-	public void testSetChococolate() {
+	public void testSetChococolateGreater() {
 		int amnt = 5;
 		myInv.setChocolate(amnt);
 		assertEquals(5, myInv.getChocolate());
-		
-		amnt = -1;
+	}
+	
+	//2. amount < 0
+	@Test
+	public void testSetChococolateLess() {
+		int amnt = -1;
 		myInv.setChocolate(amnt);
 		//assert getChocolate is still 5
-		assertEquals(5, myInv.getChocolate());
+		assertEquals(15, myInv.getChocolate());
 	}
 	
 	//test both equivalence cases for setCoffee method: 1. amount >= 0     2. amount < 0
+	
+	//amount >= 0
 	@Test
-	public void testSetCoffee() {
+	public void testSetCoffeeGreater() {
 		int amnt = 5;
 		myInv.setCoffee(amnt);
 		assertEquals(5, myInv.getCoffee());
-		
-		amnt = -1;
+	}
+	
+	//amount < 0
+	@Test
+	public void testSetCoffeeLess() {
+		int amnt = -1;
 		myInv.setCoffee(amnt);
 		//assert getCoffee is still 5
-		assertEquals(5, myInv.getCoffee());
+		assertEquals(15, myInv.getCoffee());
 	}
 	
 	//test both equivalence cases for setMilk method: 1. amount >= 0    2. amount < 0
+	
+	//amount >= 0
 	@Test
-	public void testSetMilk() {
+	public void testSetMilkGreater() {
 		int amnt = 5;
 		myInv.setMilk(amnt);
 		assertEquals(5, myInv.getMilk());
-		
-		amnt = -1;
+	}
+	
+	//test amount < 0
+	@Test
+	public void testSetMilkLess() {	
+		int amnt = -1;
 		myInv.setMilk(amnt);
 		//assert getMilk is still 5
-		assertEquals(5, myInv.getMilk());
+		assertEquals(15, myInv.getMilk());
 	}
 	
 	//test both equivalence cases for setSugar method: 1. amount >= 0    2. amount < 0
+	// amount >= 0
 	@Test
-	public void testSetSugar() {
+	public void testSetSugarGreater() {
 		int amnt = 5;
 		myInv.setSugar(amnt);
 		assertEquals(5, myInv.getSugar());
-		
-		amnt = -1;
+	}
+	
+	// amount < 0
+	@Test
+	public void testSetSugarLess() {
+		int amnt = -1;
 		myInv.setSugar(amnt);
 		//assert getSugar is still 5
-		assertEquals(5, myInv.getSugar());
+		assertEquals(15, myInv.getSugar());
 	}
 	
 	//test both equivalence cases for addChocholate method 1. amount >= 0    2. amount < 0
+	
+	//amount >= 0 
 	@Test
-	public void testAddChocolate() throws InventoryException  {
+	public void testAddChocolateGreater() throws InventoryException  {
 		String amnt = "5";
 		myInv.addChocolate(amnt);
 		//asert getChocolate == defalt amount + amnt
 		assertEquals(20, myInv.getChocolate());
+	}
 		
+	//amount < 0 make sure inventory exception is thrown
+	@Test
+	public void testAddChocolate() throws InventoryException  {
+		String amnt = "-1"; 
 
-		amnt = "-1"; 
-
-		//assert inventory exception is thrown
 		try{ 
 			myInv.addChocolate(amnt);
 			fail("InventoryException should be thrown");
@@ -93,16 +120,18 @@ public class InventoryTest {
 	
 	//test both equivalence cases for addCoffee method 1. amount >= 0    2. amount < 0
 	@Test
-	public void testAddCoffee() throws InventoryException  {
+	public void testAddCoffeeGreater() throws InventoryException  {
 		String amnt = "5";
 		myInv.addCoffee(amnt);
 		//asert getCoffee == defalt amount + amnt
 		assertEquals(20, myInv.getCoffee());
+	}
 		
+	//amount < 0 make sure inventory exception is thrown
+	@Test
+	public void testAddCoffeeLess() throws InventoryException  {
+		String amnt = "-1"; 
 
-		amnt = "-1"; 
-
-		//assert inventory exception is thrown
 		try{ 
 			myInv.addCoffee(amnt);
 			fail("InventoryException should be thrown");
@@ -114,14 +143,17 @@ public class InventoryTest {
 	
 	//test both equivalence cases for addMilk method 1. amount >= 0    2. amount < 0
 	@Test
-	public void testAddMilk() throws InventoryException  {
+	public void testAddMilkGreater() throws InventoryException  {
 		String amnt = "5";
 		myInv.addMilk(amnt);
 		//asert getMilk == defalt amount + amnt
 		assertEquals(20, myInv.getMilk());
-		
-
-		amnt = "-1"; 
+	}
+	
+	//amount < 0 make sure inventory exception is thrown
+	@Test
+	public void testAddMilkLess() throws InventoryException  {
+		String amnt = "-1"; 
 
 		//assert inventory exception is thrown
 		try{ 
@@ -136,14 +168,18 @@ public class InventoryTest {
 	//test both equivalence cases for addSugar method 1. amount >= 0    2. amount < 0
 	//Fails because of error found in method: uses a <= sign instead of >= when comparing to input to 0
 	@Test
-	public void testAddSugar() throws InventoryException  {
+	public void testAddSugarGreater() throws InventoryException  {
 		String amnt = "5";
 		myInv.addSugar(amnt);
 		//asert getSugar == defalt amount + amnt
 		assertEquals(20, myInv.getSugar());
-		
-
-		amnt = "-1"; 
+	}
+	
+	//amount < 0 make sure inventory exception is thrown
+	//fails again because of error
+	@Test
+	public void testAddSugarLess() throws InventoryException  {
+		String amnt = "-1"; 
 
 		//assert inventory exception is thrown
 		try{ 
@@ -158,7 +194,7 @@ public class InventoryTest {
 	//test 5 equivalence cases for enough ingredients method: enough of everything except 1. chocolate 2. milk 3. coffee 4. sugar
 	// 5. chocolate and coffee 6. there is enough of everything
 	@Test
-	public void testEnoughIngredients() {
+	public void testEnoughIngredientsChocolate() {
 		// create a mock recipe
 		Recipe mockRec = Mockito.mock(Recipe.class);
 		
@@ -169,47 +205,85 @@ public class InventoryTest {
 		Mockito.when(mockRec.getAmtSugar()).thenReturn(1);
 		//assert that enough ingredients returns false
 		assertEquals(false, myInv.enoughIngredients(mockRec));
-		
-		
-		//change stubbed methods so that only amtCoffee returns more than 15
-		Mockito.when(mockRec.getAmtChocolate()).thenReturn(1);
-		Mockito.when(mockRec.getAmtCoffee()).thenReturn(49);
-		//assert that enough ingredients returns false
-		assertEquals(false, myInv.enoughIngredients(mockRec));
-		
-		
-		//change stubbed methods so that only amtMilk returns more than 15
-		Mockito.when(mockRec.getAmtMilk()).thenReturn(40);
-		Mockito.when(mockRec.getAmtCoffee()).thenReturn(1);
-		//assert that enough ingredients returns false
-		assertEquals(false, myInv.enoughIngredients(mockRec));
-		
-		//change stubbed methods so that only amtSugar returns more than 15
-		Mockito.when(mockRec.getAmtMilk()).thenReturn(2);
-		Mockito.when(mockRec.getAmtSugar()).thenReturn(18);
-		//assert that enough ingredients returns false
-		assertEquals(false, myInv.enoughIngredients(mockRec));
-		
-		//change stubbed methods so that amntChocolate and amtCoffee returns more than 15
-		Mockito.when(mockRec.getAmtChocolate()).thenReturn(36);
-		Mockito.when(mockRec.getAmtCoffee()).thenReturn(49);
-		Mockito.when(mockRec.getAmtSugar()).thenReturn(4);
-		//assert that enough ingredients returns false
-		assertEquals(false, myInv.enoughIngredients(mockRec));
-		
-		//change stubbed methods so that all amnts return less than 15
-		Mockito.when(mockRec.getAmtChocolate()).thenReturn(10);
-		Mockito.when(mockRec.getAmtCoffee()).thenReturn(6);
-		//assert that enough ingredients returns false
-		assertEquals(true, myInv.enoughIngredients(mockRec));
 	}
 	
-	//test useIngredients method by passing in mock recipe that returns one for each item's get amount method
-	//and then passes it in as an argument to useIngridients and then asserts that each quantity is correct
-	//This method fails due to an error found in the method: it adds the quantity of coffee in the recipe to the 
-	//to the amount of coffee instead of subtracting it.
 	@Test
-	public void testUseIngredients() {
+	public void testEnoughIngredientsCoffee() {
+		// create a mock recipe
+		Recipe mockRec = Mockito.mock(Recipe.class);
+		
+		//set up getAmnt methods to return enough of everything (less than default inventory amnt) except coffee
+		Mockito.when(mockRec.getAmtChocolate()).thenReturn(1);
+		Mockito.when(mockRec.getAmtCoffee()).thenReturn(50);
+		Mockito.when(mockRec.getAmtMilk()).thenReturn(1);
+		Mockito.when(mockRec.getAmtSugar()).thenReturn(1);
+		//assert that enough ingredients returns false
+		assertEquals(false, myInv.enoughIngredients(mockRec));
+		
+	}
+	
+	@Test
+	public void testEnoughIngredientsMilk() {
+		// create a mock recipe
+		Recipe mockRec = Mockito.mock(Recipe.class);
+		
+		//set up getAmnt methods to return enough of everything (less than default inventory amnt) except milk
+		Mockito.when(mockRec.getAmtChocolate()).thenReturn(1);
+		Mockito.when(mockRec.getAmtCoffee()).thenReturn(1);
+		Mockito.when(mockRec.getAmtMilk()).thenReturn(50);
+		Mockito.when(mockRec.getAmtSugar()).thenReturn(1);
+		//assert that enough ingredients returns false
+		assertEquals(false, myInv.enoughIngredients(mockRec));
+	}
+	
+	@Test
+	public void testEnoughIngredientsSugar() {
+		// create a mock recipe
+		Recipe mockRec = Mockito.mock(Recipe.class);
+		
+		//set up getAmnt methods to return enough of everything (less than default inventory amnt) except sugar
+		Mockito.when(mockRec.getAmtChocolate()).thenReturn(1);
+		Mockito.when(mockRec.getAmtCoffee()).thenReturn(1);
+		Mockito.when(mockRec.getAmtMilk()).thenReturn(1);
+		Mockito.when(mockRec.getAmtSugar()).thenReturn(50);
+		//assert that enough ingredients returns false
+		assertEquals(false, myInv.enoughIngredients(mockRec));
+	}
+	
+	@Test
+	public void testEnoughIngredientsChocAndCoffee() {
+		// create a mock recipe
+		Recipe mockRec = Mockito.mock(Recipe.class);
+		
+		//set up getAmnt methods to return enough of everything (less than default inventory amnt) except chocolate
+		Mockito.when(mockRec.getAmtChocolate()).thenReturn(50);
+		Mockito.when(mockRec.getAmtCoffee()).thenReturn(50);
+		Mockito.when(mockRec.getAmtMilk()).thenReturn(1);
+		Mockito.when(mockRec.getAmtSugar()).thenReturn(1);
+		//assert that enough ingredients returns false
+		assertEquals(false, myInv.enoughIngredients(mockRec));
+	}
+	
+	@Test
+	public void testEnoughIngredientsEnoughAll() {
+		// create a mock recipe
+		Recipe mockRec = Mockito.mock(Recipe.class);
+		
+		//set up getAmnt methods to return enough of everything (less than default inventory amnt)
+		Mockito.when(mockRec.getAmtChocolate()).thenReturn(1);
+		Mockito.when(mockRec.getAmtCoffee()).thenReturn(1);
+		Mockito.when(mockRec.getAmtMilk()).thenReturn(1);
+		Mockito.when(mockRec.getAmtSugar()).thenReturn(1);
+		//assert that enough ingredients returns true
+		assertEquals(true, myInv.enoughIngredients(mockRec));
+		
+	}	
+		
+	//test useIngredients method for each item by passing in mock recipe that returns one for each item's get amount method
+	//and then passes it in as an argument to useIngridients and then asserts that the quantity is correct
+	
+	@Test
+	public void testUseIngredientsChocolate() {
 		// create a mock recipe
 		Recipe mockRec = Mockito.mock(Recipe.class);
 		
@@ -222,8 +296,55 @@ public class InventoryTest {
 		myInv.useIngredients(mockRec);
 		
 		assertEquals(14, myInv.getChocolate());
+	}
+	
+	//This method fails due to an error found in the method: it adds the quantity of coffee in the recipe to the 
+	//to the amount of coffee instead of subtracting it.
+	@Test
+	public void testUseIngredientsCoffee() {
+		// create a mock recipe
+		Recipe mockRec = Mockito.mock(Recipe.class);
+		
+		//set up getAmnt methods to return 1 for every item
+		Mockito.when(mockRec.getAmtChocolate()).thenReturn(1);
+		Mockito.when(mockRec.getAmtCoffee()).thenReturn(1);
+		Mockito.when(mockRec.getAmtMilk()).thenReturn(1);
+		Mockito.when(mockRec.getAmtSugar()).thenReturn(1);
+		
+		myInv.useIngredients(mockRec);
+		
 		assertEquals(14, myInv.getCoffee());
+	}
+	
+	@Test
+	public void testUseIngredientsMilk() {
+		// create a mock recipe
+		Recipe mockRec = Mockito.mock(Recipe.class);
+		
+		//set up getAmnt methods to return 1 for every item
+		Mockito.when(mockRec.getAmtChocolate()).thenReturn(1);
+		Mockito.when(mockRec.getAmtCoffee()).thenReturn(1);
+		Mockito.when(mockRec.getAmtMilk()).thenReturn(1);
+		Mockito.when(mockRec.getAmtSugar()).thenReturn(1);
+		
+		myInv.useIngredients(mockRec);
+		
 		assertEquals(14, myInv.getMilk());
+	}
+	
+	@Test
+	public void testUseIngredientsSugar() {
+		// create a mock recipe
+		Recipe mockRec = Mockito.mock(Recipe.class);
+		
+		//set up getAmnt methods to return 1 for every item
+		Mockito.when(mockRec.getAmtChocolate()).thenReturn(1);
+		Mockito.when(mockRec.getAmtCoffee()).thenReturn(1);
+		Mockito.when(mockRec.getAmtMilk()).thenReturn(1);
+		Mockito.when(mockRec.getAmtSugar()).thenReturn(1);
+		
+		myInv.useIngredients(mockRec);
+		
 		assertEquals(14, myInv.getSugar());
 	}
 }
